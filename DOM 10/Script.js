c63bfcaceff7df9d37b981b246dea84ffa69f6d1
Limaygitshub <you@example.com>
2023-03-10 08:23:19 +0530

    Dom 10

--- DOM 10/Script.js
+++ DOM 10/Script.js
@@ -1,14 +1,15 @@
-let a= document.querySelector('#luxury')
+//by Id
+let a= document.querySelector('#luxury')        //to get single element
 console.log(a);
 
-let a2 = document.querySelectorAll('#luxury')
+let a2 = document.querySelectorAll('#luxury')       //to get all element
 console.log(a2);
 
-  
-let b= document.querySelector('.car')
+//by class
+let b= document.querySelector('.car')    //to get single element
 console.log(a);
 
-let b2 = document.querySelectorAll('.car')
+let b2 = document.querySelectorAll('.car')  //to get all element
 console.log(a2)
 
 
